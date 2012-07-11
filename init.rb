f5f1b09ea13d524a8c17f545c916745c2d1eaf42
dgipp <dgipp@cisco.com>
2012-07-11 14:06:33 -0700

    explicitly copy assets on startup (fixes issue on windows servers) Fix from Arusarka Haldar on Throughtworks Communities site: http://community.thoughtworks.com/posts/bce5f8b69b

--- init.rb
+++ init.rb
@@ -8,3 +8,16 @@ end
 if defined?(RAILS_ENV) && RAILS_ENV == 'production' && defined?(MinglePlugins)
   MinglePlugins::Macros.register(BurndownChartFlot, 'burndown_chart_flot')
 end 
+
+require 'fileutils'
+
+module InitUtils
+  def InitUtils.deploy_public_files(macro)
+    deploy_dir = 'public/plugin_assets/'+macro
+    FileUtils.mkpath 'public/plugin_assets'
+    FileUtils.rm_r deploy_dir if File.exists? deploy_dir
+    FileUtils.cp_r 'vendor/plugins/'+macro+'/assets', deploy_dir
+  end
+end
+
+InitUtils.deploy_public_files 'burndown_chart_flot'
